13a85607e280006289e8e226e68c3996b9fdb149
Jared Ning <jared@redningja.com>
2017-08-03 21:37:48 -0500

    Update fandango (https).

--- test/controllers/showtime_lists_controller_test.rb
+++ test/controllers/showtime_lists_controller_test.rb
@@ -3,18 +3,20 @@ require 'test_helper'
 class ShowtimeListsControllerTest < ActionDispatch::IntegrationTest
 
   test 'makes request to find showtimes for theater on given day' do
-    VCR.use_cassette 'fandango_theater_showtimes_amcquail' do
-      theater = Theater.create!(
-        :remote_id => 'aaktw',
-        :name      => "AMC Quail Springs Mall 24",
-      )
+    travel_to DateTime.new(2017, 1, 5) do
+      VCR.use_cassette 'fandango_theater_showtimes_amcquail' do
+        theater = Theater.create!(
+          :remote_id => 'aaktw',
+          :name      => "AMC Quail Springs Mall 24",
+        )
 
-      assert_difference 'theater.showtime_lists.count' do
-        get theater_showtime_lists_path(theater)
+        assert_difference 'theater.showtime_lists.count' do
+          get theater_showtime_lists_path(theater)
+        end
+
+        showtime_list = theater.showtime_lists.first
+        assert_equal 22, showtime_list.movies.size
       end
-
-      showtime_list = theater.showtime_lists.first
-      assert_equal 22, showtime_list.movies.size
     end
   end
 
